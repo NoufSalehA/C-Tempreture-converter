7770efe903af099e283c481a5a517b10769796dd
NoufSalehA <noufsalehc23@gmail.com>
2024-03-19 11:45:37 +0300

    converter of temperature

--- src/Program.cs
+++ src/Program.cs
@@ -1,1 +1,84 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Security.Cryptography;//take 2 inputs from user temperature and the type of it C or F
+
+class Program
+{
+    public static string TempConvert(double tempAmount, string unit)
+    {
+
+        if (unit.ToUpper() == "F")
+        {
+            tempAmount = (tempAmount - 32) / 1.8;//to celsius
+            unit = "C";
+            return $"{tempAmount}  {unit}";
+        }
+        if (unit.ToUpper() == "C")
+        { //to fahrenheit
+            tempAmount = (tempAmount * 1.8) + 32;
+            unit = "F";
+            return $"{tempAmount}  {unit}";
+
+        }
+
+        else
+        {
+            Console.WriteLine("Invalid value");
+            return string.Empty;
+        }
+
+    }
+    public static void Main(string[] args)
+    {
+        try
+        {
+            while (true)
+            {
+                Console.WriteLine("Enter temperature and it is unit ,type C for celsius, type F for fahrenheit,to Exit enter quit");
+
+                string info = Console.ReadLine() ?? "";
+                if (info.ToLower() == "quit")
+                {
+                    break;
+                }
+                string[] arr = info.Split(" ");
+                if (arr.Length != 2)
+                {
+                    Console.WriteLine("Invalid input");
+                    continue;
+
+
+                }
+
+                if (double.TryParse(arr[0], out double tempNum))//Convert it to double
+                {
+                    string theUnit = arr[1].ToUpper();
+
+                    Console.WriteLine(TempConvert(tempNum, theUnit));
+                }
+
+                else
+                {
+                    Console.WriteLine("--Invalid --Please Enter A numeric temperature value");
+
+                }
+            }
+        }
+
+        catch (ArgumentException e)
+        {
+            Console.WriteLine(e.Message);
+        }
+
+
+
+
+
+
+
+
+    }
+}
+
+
+
+
